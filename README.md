db4e77ffdbbf2030d53fa667cc544eeaa1aba76f
Mark Shust <mark@shust.com>
2015-08-20 09:22:53 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Magento 2 Foo_Bar Modules
+This repository contains code examples of Magento 2 modules.
+
+## Module Examples
+* `block-template`: Create a block and display in template HTML
---
+++ block-template/Block/Baz.php
@@ -0,0 +1,14 @@
+<?php
+namespace Foo\Bar\Block;
+
+/**
+* Baz block
+*/
+class Baz
+    extends \Magento\Framework\View\Element\Template
+{
+    public function getTitle()
+    {
+        return "Foo Bar Baz";
+    }
+}
